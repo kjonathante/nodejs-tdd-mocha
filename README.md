3688a183625da5416f794f2a2cd6ccd3cae041b7
Kit Jonathan Te <kjonathante@gmail.com>
2019-05-04 12:21:55 -0700

    Added eslint

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # nodejs-boilerplate
+
+### eslint
+```
+npm install -g --save-dev eslint
+eslint --init 
+# select config in js
+# .eslintrc.js
+```
--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   googleId: process.env.GOOGLE_ID,
   appleId: process.env.APPLE_ID
-}
+};
